7a9203c9429b34593182242d50723b581838a6a2
Isaac Zais <isaaczais@hotmail.com>
2015-04-17 15:54:12 -0500

    Created the messaging UI.
    Created the messaging UI so conversations look similar to that of other
    messaging applications. Added a bunch of drawables. Moved most hardcoded
    size values to dimens folders for better UI on different devices. Added
    A Utils class for generating IDs. Created a very Dummy Model for a
    Message.

--- app/src/main/java/com/ianofferdahl/shitchat/ChatActivity.java
+++ app/src/main/java/com/ianofferdahl/shitchat/ChatActivity.java
@@ -1,11 +1,21 @@
 package com.ianofferdahl.shitchat;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ianofferdahl.shitchat.DataModels.TestObject;
 
@@ -15,10 +25,45 @@ import org.springframework.web.client.RestTemplate;
 
 public class ChatActivity extends Activity {
 
+    private double maxWidth;
+
+    private RelativeLayout chatLayout;
+
+    private View lastMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        maxWidth = dm.widthPixels/1.2;
+
+        this.chatLayout = (RelativeLayout) findViewById(R.id.chat_layout);
+        this.addPartnerMessage("Hello fellow Pooper");
+        this.addMessage("Hello back to you");
+        this.addPartnerMessage("What did you eat?");
+        this.addMessage("Cheerios and Vodka");
+        this.addMessage("and applesauce");
+        this.addPartnerMessage("Hello fellow Pooper");
+        this.addMessage("Hello back to you");
+        this.addPartnerMessage("What did you eat?");
+        this.addMessage("Cheerios and Vodka");
+        this.addMessage("and applesauce");
+        this.addPartnerMessage("Hello fellow Pooper");
+        this.addMessage("Hello back to you");
+        this.addPartnerMessage("What did you eat?");
+        this.addMessage("Cheerios and Vodka");
+        this.addMessage("and applesauce");
+        this.addPartnerMessage("Hello fellow Pooper");
+        this.addMessage("Hello back to you");
+        this.addPartnerMessage("What did you eat?");
+        this.addPartnerMessage("Cheerios and Vodka");
+        this.addPartnerMessage("and applesauce as well as a plethora of other things, This is really long text. Is is tho?");
+        this.addMessage("What did you eat?");
+        this.addMessage("Cheerios and Vodka");
+        this.addMessage("and applesauce as well as a plethora of other things, This is really long text. Is is tho?");
     }
 
     @Override
@@ -47,6 +92,68 @@ public class ChatActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addPartnerMessage(String message) {
+
+        TextView partnerMessage = new TextView(this);
+        partnerMessage.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        partnerMessage.setText(message);
+        partnerMessage.setPadding(16, 16, 16, 16);
+        partnerMessage.setTextColor(getResources().getColor(R.color.white));
+        partnerMessage.setGravity(Gravity.CENTER);
+        partnerMessage.setBackgroundResource(R.drawable.partner_message_background);
+        partnerMessage.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.chat_text_size));
+        partnerMessage.setMaxWidth((int) maxWidth);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0,4,0,4);
+        params.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            partnerMessage.setId(Utils.generateViewId());
+        } else {
+            partnerMessage.setId(View.generateViewId());
+        }
+
+        if (lastMessage != null) {
+            params.addRule(RelativeLayout.BELOW, lastMessage.getId());
+        }
+
+        lastMessage = partnerMessage;
+        chatLayout.addView(partnerMessage, params);
+
+    }
+
+    private void addMessage(String message) {
+
+        TextView sentMessage = new TextView(this);
+        sentMessage.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        sentMessage.setText(message);
+        sentMessage.setPadding(16, 16, 16, 16);
+        sentMessage.setTextColor(getResources().getColor(R.color.white));
+        sentMessage.setBackgroundResource(R.drawable.message_background);
+        sentMessage.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.chat_text_size));
+        sentMessage.setMaxWidth((int) maxWidth);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            sentMessage.setId(Utils.generateViewId());
+        } else {
+            sentMessage.setId(View.generateViewId());
+        }
+
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0,4,0,4);
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+
+        if (lastMessage != null) {
+            params.addRule(RelativeLayout.BELOW, lastMessage.getId());
+        }
+
+        lastMessage = sentMessage;
+        chatLayout.addView(sentMessage, params);
+
     }
 
     private void testObjectReceived(TestObject testObject) {
---
+++ app/src/main/java/com/ianofferdahl/shitchat/DataModels/Message.java
@@ -0,0 +1,10 @@
+package com.ianofferdahl.shitchat.DataModels;
+
+/**
+ * Created by Isaac on 4/17/2015.
+ */
+public class Message {
+
+    private String text;
+
+}
